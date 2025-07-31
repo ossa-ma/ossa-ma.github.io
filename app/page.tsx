94691f74c5cf7da8ce4dd0fc42ca194a42b211da
ossa-ma <amassosaa@gmail.com>
2025-08-01 00:46:10 +0100

    Add first blog post

--- app/page.tsx
+++ app/page.tsx
@@ -9,19 +9,20 @@ export default function Page() {
       </h1>
       <p className="mb-4">
         I am a software engineer with a First Class MEng in Computing from
-        Imperial College London, and I am passionate about transitioning into a
-        career in secondary Computer Science education. My professional
-        experience at companies like Fitch Group and Shell has given me a deep,
-        practical understanding of software engineering, data science, and
-        machine learning, which I am eager to share with the next generation of
-        innovators.
+        Imperial College London.
+        {/* , and I am passionate about transitioning into a */}
+        {/* career in secondary Computer Science education. My professional */}
+        {/* experience at companies like Fitch Group and Shell has given me a deep, */}
+        {/* practical understanding of software engineering, data science, and */}
+        {/* machine learning, which I am eager to share with the next generation of */}
+        {/* innovators. */}
       </p>
       <p className="mb-4">
         Beyond my professional life, I am deeply committed to martial arts. I
-        train and spar regularly in MMA, wrestling, BJJ, and Muay Thai, which has
-        instilled in me a strong sense of discipline and resilience. I am also
-        an avid tennis player, and I find that the strategic thinking required on
-        the court complements my problem-solving skills as an engineer.
+        train and spar regularly in MMA, wrestling, BJJ, and Muay Thai. I am also
+        an avid tennis player.
+        {/* , and I find that the strategic thinking required on */}
+        {/* the court complements my problem-solving skills as an engineer. */}
       </p>
       <p className="mb-4">
         This website is a place for me to share my thoughts on technology,
