36a4cb69c7265e9b7243b8ca9d86a0fee06110c9
ossa-ma <ossa-ma@users.noreply.github.com>
2026-01-07 20:13:51 +0000

    I want whatsapp previews

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
     type: 'website',
     images: [
       {
-        url: `${baseUrl}/og.png`,
+        url: `${baseUrl}/og.jpg`,
         width: 1200,
         height: 630,
         alt: 'Ossama Chaib',
